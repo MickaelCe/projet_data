e98707832e8bf0ce2cbff3e85031822563a4759a
Oscar Millot <oscarmillot23@gmail.com>
2021-08-24 15:29:11 +0200

    modif cdd pge infoooo

--- public/model/model_infos.php
+++ public/model/model_infos.php
@@ -1,6 +1,6 @@
 <?php
 
-$req = "SELECT img_url FROM steam_game ORDER BY RAND ( ) LIMIT 5";
+$req = "SELECT img_url FROM steam_game ORDER BY RAND ( ) LIMIT 6";
 $exe = [];
 
 $req_two = "SELECT * FROM steam_game WHERE id LIKE :id LIMIT 1";
